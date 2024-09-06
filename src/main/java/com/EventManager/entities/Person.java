16fec17e2e1528090290976f6e8b989a1ab2155c
pedro9401 <paulo-ppaa@hotmail.com>
2024-09-06 14:22:53 -0300

    feat: creating entity for person

---
+++ src/main/java/com/EventManager/entities/Person.java
@@ -0,0 +1,47 @@
+package com.EventManager.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "people")
+@EqualsAndHashCode(of="id")
+
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "full_name", nullable = false)
+    @Size(min = 5, message = "Full name must be at least 5 characters long")
+    private String fullName;
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    @Column(name = "work_field")
+    private String workField;
+
+    @Column(name = "cpf")
+    @Size(min = 11, message = "CPF must be at least 11 characters long")
+    private String cpf;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    @Size(min = 10, message = "Phone number must be at least 10 characters long")
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "postal_code")
+    @Size(min = 8, message = "Postal code must be at least 8 characters long")
+    private String postalCode;
+}
