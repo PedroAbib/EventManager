cbea1bbbc460f07d5e9a86d2c06514351da43cc4
pedro9401 <paulo-ppaa@hotmail.com>
2024-09-07 13:21:33 -0300

    feat: implemented the method addPerson through the Person controller and services

--- src/main/java/com/EventManager/controllers/PersonController.java
+++ src/main/java/com/EventManager/controllers/PersonController.java
@@ -1,12 +1,12 @@
 package com.EventManager.controllers;
 
 import com.EventManager.entities.Person;
+import com.EventManager.entities.PersonRecord;
 import com.EventManager.services.PersonServices;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,9 @@ public class PersonController {
         return ResponseEntity.ok(allPerson);
     }
 
-
+    @PostMapping
+    public ResponseEntity<Person> addPerson(@RequestBody @Valid PersonRecord data) {
+        Person newPerson = personServices.addPerson(data);
+        return ResponseEntity.ok(newPerson);
+    }
 }
