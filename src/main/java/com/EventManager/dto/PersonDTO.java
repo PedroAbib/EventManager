f3e5264fd70d376758fcfbd9f87399c3cf491e91
pedro <paulo-ppaa@hotmail.com>
2024-09-25 13:04:27 -0300

    refactor: PersonDTO now can receive any or all Person properties

--- src/main/java/com/EventManager/dto/PersonDTO.java
+++ src/main/java/com/EventManager/dto/PersonDTO.java
@@ -2,7 +2,11 @@ package com.EventManager.dto;
 
 public record PersonDTO(
         String fullName,
+        String tagName,
+        String workField,
         String cpf,
+        String email,
         String phoneNumber,
+        String address,
         String postalCode) {
 }
